7e29a6ba75e4a4ff3c95d06548edfc258cb500f5
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-27 07:36:49 -0500

    feat(User): add base class User to inherits

---
+++ src/main/java/com/hotelNova/models/User.java
@@ -0,0 +1,62 @@
+package com.hotelNova.models;
+
+public abstract class User {
+
+    private String id;
+    private String username;
+    private String password;
+    private String role;
+
+    public User (String username, String password, String role){
+
+        setUsername(username);
+        this.password = password;
+        setRole(role);
+
+    }
+
+    public User (String id, String username, String password, String role) {
+
+        this(username, password, role);
+        this.id = id;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setUsername(String username) {
+
+        if (username == null || !username.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$")){
+
+            throw new IllegalArgumentException("Invalid name: the name can't have numbers");
+
+        }
+
+        this.username = username;
+
+    }
+
+    public void setRole(String role) {
+
+        if (role == null || !(role.equals("ADMIN") || role.equals("RECEPTIONIST") || role.equals("GUEST"))) {
+            throw new IllegalArgumentException("Invalid role: Must be ADMIN, RECEPTIONIST, or GUEST.");
+        }
+
+        this.role = role;
+    }
+
+}
