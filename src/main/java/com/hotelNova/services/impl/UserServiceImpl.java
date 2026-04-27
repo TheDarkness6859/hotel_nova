9d82da8c7618e9f3948ecb60f295af08a7232b0a
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-27 11:48:21 -0500

    feat(UserServiceImpl): use implementations of interface and business logic

---
+++ src/main/java/com/hotelNova/services/impl/UserServiceImpl.java
@@ -0,0 +1,116 @@
+package com.hotelNova.services.impl;
+
+import com.hotelNova.dao.impl.GenericDAOImpl;
+import com.hotelNova.dao.impl.UserDAOImpl;
+import com.hotelNova.db.DatabaseConnection;
+import com.hotelNova.models.User;
+import com.hotelNova.services.GenericService;
+import com.hotelNova.services.UserService;
+import com.hotelNova.utils.HashedPassword;
+import com.hotelNova.utils.LogManager;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    private final GenericDAOImpl<User> userDAO = new UserDAOImpl();
+
+    @Override
+    public List<User> findAll () {
+
+        return userDAO.findAll();
+
+    }
+
+    @Override
+    public Optional<User> findByIdOrName (String value) {
+
+        return userDAO.findByIdOrName(value);
+
+    }
+
+    @Override
+    public boolean save (User user) {
+
+        try {
+
+            DatabaseConnection.startTransaction();
+
+            boolean success = userDAO.save(user);
+
+            if (success) {
+
+                DatabaseConnection.commit();
+                LogManager.addLog("INFO", "Commit created correctly");
+                return true;
+
+            }else {
+
+                DatabaseConnection.rollback();
+                LogManager.addLog("WARNING", "Create rollback to save data");
+                return false;
+
+            }
+
+        }catch (SQLException err){
+
+            DatabaseConnection.rollback();
+            LogManager.addLog("ERROR", "Error saving user: " + err.getMessage());
+            return false;
+
+        }
+
+    }
+
+    @Override
+    public boolean edit(User user) {
+
+        try {
+
+            DatabaseConnection.startTransaction();
+
+            boolean success = userDAO.edit(user);
+
+            if (success) {
+
+                DatabaseConnection.commit();
+                LogManager.addLog("INFO", "User updated and commit executed correctly");
+                return true;
+
+            } else {
+
+                DatabaseConnection.rollback();
+                LogManager.addLog("WARNING", "User update failed, rollback executed");
+                return false;
+
+            }
+
+        } catch (SQLException err) {
+
+            DatabaseConnection.rollback();
+            LogManager.addLog("ERROR", "Error updating user: " + err.getMessage());
+            return false;
+
+        }
+
+    }
+
+    @Override
+    public boolean delete (String id) {
+
+        return userDAO.delete(id);
+
+    }
+
+    @Override
+    public Optional<User> login(String username, String password){
+
+        return userDAO.findByIdOrName(username)
+                .filter(user -> HashedPassword.checkPassword(password, user.getPassword())
+        );
+
+    }
+
+}
